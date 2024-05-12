c045bce923d2578b81735fb25bd0c7752bb22348
Stuart Allan Rutherford <stalruth@gmail.com>
2024-05-12 15:37:17 +1000

    Add favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -61,6 +61,10 @@ async function submitWebhook(e) {
   <meta property="og:url" content="https://ots-notifier.stalruth.dev/" />
   <meta property="og:description" content="Receive a Discord message when the Open Teamlists from a major event are published." />
   <meta name="description" content="Receive a Discord message when the Open Teamlists from a major event are published." />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
 </svelte:head>
 
 <main>
